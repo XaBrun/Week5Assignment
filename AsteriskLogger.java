8bdecf4bd744eb6a9301b363537dd93dbbbd1b2f
XaBrun <134178329+XaBrun@users.noreply.github.com>
2023-06-03 23:17:18 -0400

    AsteriskLogger.java
    Added comments

--- AsteriskLogger.java
+++ AsteriskLogger.java
@@ -4,13 +4,13 @@ public class AsteriskLogger implements Logger {
 
 	
 	public void log(String log) {
-		System.out.println("***" + log + "***");
+		System.out.println("***" + log + "***"); // surround input of log with 3 asterisks on eeach side
 		
 	}
 
 	
 	public void error(String error) {
-		System.out.println("******************\n***Error:" + error + "***\n******************");
+		System.out.println("******************\n***Error:" + error + "***\n******************"); // surround input of error with a box of asterisks
 		
 	}
 
