803f51eb813efa5884d5abcb6a36cf15cb213c57
XaBrun <134178329+XaBrun@users.noreply.github.com>
2023-06-03 23:16:18 -0400

    SpacedLogger.java
    Added comments

--- SpacedLogger.java
+++ SpacedLogger.java
@@ -2,18 +2,18 @@ package Week5Assignment;
 
 public class SpacedLogger implements Logger {
 	
-	public void log(String log) {
+	public void log(String log) { //take input of String
 		
-		System.out.println(log.replace("", " ").trim());
+		System.out.println(log.replace("", " ").trim()); // replace empty strings with space, trim to remove leading and trailing spaces
 			
 		}
 		
 
 
 	
-	public void error(String error) {
+	public void error(String error) { //take input of String
 		
-		System.out.println("ERROR: " +  error.replace("", " ").trim());
+		System.out.println("ERROR: " +  error.replace("", " ").trim()); // replace empty strings with space, trim to remove leading and trailing spaces
 		
 		
 	}
